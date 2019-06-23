edea19a9417c19ba8cddaf46fae2c914bc42e497
hemantdada <hemantpatil4@gmail.com>
2019-06-24 00:39:35 +0530

    version1

--- src/app/core/components/home/home.component.spec.ts
+++ src/app/core/components/home/home.component.spec.ts
@@ -1,34 +1,15 @@
+// import { SharedModule } from './shared.module';
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { HomeComponent } from './home.component';
+// describe('shared', () => {
+//   describe('SharedModule', () => {
+//     let sharedModule: SharedModule;
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+//     beforeEach(() => {
+//       sharedModule = new SharedModule();
+//     });
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it(`should have as title 'my-app'`, async(() => {
-    expect(component.title).toEqual('my-app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const compiled: any = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-app!');
-  }));
-});
+//     it('should create an instance', () => {
+//       expect(sharedModule).toBeTruthy();
+//     });
+//   });
+// });
