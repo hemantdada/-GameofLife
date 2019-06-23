11ba0f93a416e798412445928480d63c110e5f0c
hemantdada <hemantpatil4@gmail.com>
2019-06-24 00:41:30 +0530

    version1

--- src/app/core/components/home/home.component.spec.ts
+++ src/app/core/components/home/home.component.spec.ts
@@ -1,15 +1,31 @@
-// import { SharedModule } from './shared.module';
 
-// describe('shared', () => {
-//   describe('SharedModule', () => {
-//     let sharedModule: SharedModule;
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { HomeComponent } from './home.component';
 
-//     beforeEach(() => {
-//       sharedModule = new SharedModule();
-//     });
+// describe('HomeComponent', () => {
+//   let component: HomeComponent;
+//   let fixture: ComponentFixture<HomeComponent>;
 
-//     it('should create an instance', () => {
-//       expect(sharedModule).toBeTruthy();
-//     });
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ HomeComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(HomeComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
 //   });
+
+//   // it('should create', () => {
+//   //   expect(component).toBeTruthy();
+//   // });
+
+//   // it('should render title in a h1 tag', async(() => {
+//   //   const compiled: any = fixture.debugElement.nativeElement;
+//   //   expect(compiled.querySelector('h1').textContent)
+//   //     .toContain('This is the home component of Lazy loaded module');
+//   // }));
 // });
